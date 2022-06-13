826dfc6d364b8959e1856f24a2c93979a420b954
tap1oca boy <tap1ocaboy.uwu@gmail.com>
2022-06-14 01:36:45 +0200

    updates idk kill me

--- main.js
+++ main.js
@@ -1,28 +1,45 @@
-const Discord = require('discord.js');
-require("dotenv").config();
+const Discord = require('discord.js')
+require("dotenv").config()
 
-var cockamount = 0;
+const generateImage = require("./generateImage")
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES"
     ]
-});
+})
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
-});
+})
 
 client.on("messageCreate", (message) => {
     if (message.content == "!cock") {
-        message.send(`<@${member.id}> I love huge penis`);
+        message.reply("I love penis! ^~^")
     }
 })
 
-const welcomeChannelId = "985128570592591952";
-client.on("guildMemberAdd", (member) => {
-    member.guild.channels.channelCountWithoutThreads.get(welcomeChannelId).send(`<@${member.id}> Hello`)
-});
+client.on("messageCreate", (message) => {
+    if (message.content == "hi fembot") {
+        message.reply("Hello!! :3")
+    }
+})
 
-client.login(process.env.TOKEN);
+client.on("messageCreate", (message) => {
+    if (message.content == "fuck you fembot") {
+        message.reply("you should kill yoursel")
+    }
+})
+
+const welcomeChannelId = "985128570592591952"
+
+client.on("guildMemberAdd", async (member) => {
+    const image = await generateImage(member)
+    member.guild.channels.channelCountWithoutThreads.get(welcomeChannelId).send({
+        content: `<@${member.id}> Hello`,
+        files: [img]
+    })
+})
+
+client.login(process.env.TOKEN)
